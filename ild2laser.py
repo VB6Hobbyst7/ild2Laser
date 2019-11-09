b9095bf66992b20fb802e1a034a270a28270b30c
th3cookie <th3cookie@gmail.com>
2019-11-09 23:12:31 +1100

    Update ild2laser.py

--- ild2laser.py
+++ ild2laser.py
@@ -14,11 +14,7 @@ def remove_non_ascii(text):
 
 def copy_files():
     # Initializing
-    arr = []
     count = 0
-
-    # Array of items in the directory
-    files = [f for f in os.listdir('To_Do')]
 
     for f in files:
         # If the file ends in .ild (case insensitive), it will proceed...
@@ -33,27 +29,26 @@ def copy_files():
             newFileName = remove_non_ascii(newFileName)
 
             # If there are duplicate names going to be written, change the name with an increasing count integer substitution
-            if len(arr) == 0 or not any(x in newFileName for x in arr):
-                arr.append(newFileName)
+            if len(files_arr) == 0 or not any(x in newFileName for x in files_arr):
+                files_arr.append(newFileName)
             else:
                 newFileName = f"{newFileName[0:5]}{count}.ild"
                 count += 1
-                arr.append(newFileName)
+                files_arr.append(newFileName)
         else:
             print(f"{f} is not a valid .ild file and cannot be processed.")
         
         # Checks if the file already exists, copies if it doesn't
-        if not os.path.exists(f'./Processed/ildloves/{newFileName}'):
-            shutil.copy(f'./To_Do/{f}', f'./Processed/ildloves/{newFileName}')
+        if not os.path.exists(f'./Processed/ildfiles/{newFileName}'):
+            shutil.copy(f'./To_Do/{f}', f'./Processed/ildfiles/{newFileName}')
 
 def generate_playlist():
-    playlist_file = f'{PlayList_dir}/{playlist_filename}'
+    playlist_contents = ''
     if os.path.exists(playlist_file):
         with open(playlist_file, "r") as f:
-            contents = f.readlines()
-            print(contents)
+            playlist_contents = f.readlines()
             new_arr = []
-            for x in contents:
+            for x in playlist_contents:
                 try:
                     ct = re.search("^\d*", x)
                     new_arr.append(int(ct.group()))
@@ -67,16 +62,18 @@ def generate_playlist():
         count = 1
     
     with open(playlist_file, "a+") as f:
-        f.write(f"\n{count},({effect_filename})")
+        if not any(effect_filename in s for s in playlist_contents):
+            f.write(f"{count},({effect_filename})\n")
 
 def generate_effects():
-    effect_file = f'{PlayList_dir}/{effect_filename}'
+    effects_contents = ''
+    no_exists_array = []
+    count = 1
     if os.path.exists(effect_file):
         with open(effect_file, "r") as f:
-            contents = f.readlines()
-            print(contents)
+            effects_contents = f.readlines()
             new_arr = []
-            for x in contents:
+            for x in effects_contents:
                 try:
                     ct = re.search("^\d*", x)
                     new_arr.append(int(ct.group()))
@@ -86,19 +83,28 @@ def generate_effects():
                 count = max(new_arr) + 1
             except:
                 count = 1
+
     else:
         count = 1
-    
-    toteffects = ''
+
+    total_effects = ''
     for effect in effect_vars:
         if effect_vars.get(effect):
-            toteffects += f"{effect}={effect_vars[effect]}, "
-    print(toteffects)
+            total_effects += f"{effect}={effect_vars[effect]},"
 
-    # with open(effect_file, "w") as f:
-    #     for i in contents:
-    #         f.write(i)
-    #     f.write(f"\n{count},({effect_filename})")
+    if os.path.exists(effect_file):
+        with open(effect_file, "w+") as f:
+            print(effects_contents)
+            for i in effects_contents:
+                f.write(i)
+
+    with open(effect_file, "a+") as f:
+        for file in files_arr:
+            if not any(file in s for s in effects_contents):
+                no_exists_array.append(file)
+        for item in no_exists_array:
+            f.write(f"{count},(ildfiles/{item},{total_effects})\n")
+            count += 1
 
 if __name__ == "__main__":
 
@@ -114,7 +120,7 @@ if __name__ == "__main__":
     # Variables that alter how the item is displayed (leave empty if none required)
     effect_vars = {
     # Time
-    'TI': 'YES',
+    'TI': '5',
     # 
     'SI': '22',
     # Rotation
@@ -124,7 +130,7 @@ if __name__ == "__main__":
     # 
     'HR': '',
     # 
-    'HB': '',
+    'HB': '23',
     # 
     'VR': '',
     # 
@@ -141,17 +147,23 @@ if __name__ == "__main__":
 
     cwd = os.getcwd()
     to_do_dir = f'{cwd}/To_Do'
-    ildloves_dir = f'{cwd}/Processed/ildloves'
+    ildfiles_dir = f'{cwd}/Processed/ildfiles'
     PlayList_dir = f'{cwd}/Processed/PlayList'
+    effect_file = f'{PlayList_dir}/{effect_filename}'
+    playlist_file = f'{PlayList_dir}/{playlist_filename}'
 
     if not os.path.exists(to_do_dir):
         os.makedirs(to_do_dir)
         sys.exit(f"Please put your .ild files in the directory {cwd}/To_Do and then re-run this program.")
-    if not os.path.exists(ildloves_dir):
-        os.makedirs(ildloves_dir)
+    if not os.path.exists(ildfiles_dir):
+        os.makedirs(ildfiles_dir)
     if not os.path.exists(PlayList_dir):
         os.makedirs(PlayList_dir)
 
-    # copy_files()
-    # generate_playlist()
+    # Array of items in the To_Do directory
+    files = [f for f in os.listdir('To_Do')]
+    files_arr = []
+
+    copy_files()
+    generate_playlist()
     generate_effects()
